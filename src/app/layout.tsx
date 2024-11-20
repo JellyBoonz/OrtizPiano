6106f246f013a736a3c2faaa3a3618273594729e
Jaiden Ortiz <jaidenortiz@Jaidens-MacBook-Pro-2.local>
2024-11-20 17:29:59 -0500

    Add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,39 @@
 'use client';
+
 import { ReactNode, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
+import Link from 'next/link'
 import './globals.css';
-import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
+  const shouldIncludeScript = pathname === '/' || pathname === '/schedule';
+
   return (
     <html lang="en">
       <body className="min-h-screen bg-background text-foreground">
+        {shouldIncludeScript && (
+          <>
+            <Script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=AW-16763613402"
+            />
+            <Script id="google-tag">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16763613402');
+              `}
+            </Script>
+          </>
+        )}
         <div>
           <MobileNav/>
-          <main className="flex flex-col items-center p-10">
-            {children}
-          </main>
+          <main className="flex flex-col items-center p-10">{children}</main>
           <footer className="p-5 text-center border-t border-subtle">
             <p className="text-secondary">© 2024 Ortiz Piano Tuning</p>
           </footer>
