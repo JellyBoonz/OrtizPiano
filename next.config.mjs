0815064745f8515bc7a4bd5190395f80d7668fae
Jaiden Ortiz <jaidenortiz@Jaidens-MacBook-Pro-2.local>
2025-05-15 17:33:38 -0400

    Favicon...

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,27 @@
 const nextConfig = {
     transpilePackages: ['framer-motion'],
     webpack(config) {
-
         config.module.rules.push({
             test: /\.svg$/,
             use: ["@svgr/webpack"],
         });
         return config;
+    },
+    images: {
+        domains: ['ortizpiano.com'],
+    },
+    async headers() {
+        return [
+            {
+                source: '/piano.webp',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ];
     },
 };
 
